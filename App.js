639f3697ee8141e77481493e8322a56098dbd34f
Aravind <aravind16101800@gmail.com>
2024-05-03 21:57:01 +0530

    feat: episode2-Igniting_our_app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement("div",{id:"parent"},
