cb242f3f543fffc0aaa1ec5b3b73bf7d04dff5b6
Aravind <aravind16101800@gmail.com>
2024-05-07 22:10:16 +0530

    feat: useState() Hook-filtered top rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 
 const Body = () => {
+  // Local State Variable
+  const [listOfRestaurants,setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" 
+        onClick={() => {
+          let filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+          setListOfRestaurants(filteredList);
+        }}
+        >Top Rated Restaurant</button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
