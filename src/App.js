cfdb6954ca04bc5cf6d4185ca0fba0108dbe060f
Aravind <aravind16101800@gmail.com>
2024-05-09 15:14:20 +0530

    feat: children route, link component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter , RouterProvider} from "react-router-dom";
+import { createBrowserRouter , RouterProvider, Outlet} from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+
 
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -20,18 +21,24 @@ const AppLayout = () => {
 // creating configuration
 const appRoute = createBrowserRouter([
   {
-    path : "/",
-    element: <AppLayout/>,
-    errorElement: <Error/>
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body/>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
   },
-  {
-    path : "/about",
-    element: <About/>
-  },
-  {
-    path: "/contact",
-    element: <Contact/>
-  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
@@ -13,20 +13,30 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src= {LOGO_URL}
-        ></img>
+        <img className="logo" src={LOGO_URL}></img>
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
-          <button className="login" onClick={()=>{
-            btnNameReact === "Login"?setBtnNameReact("Logout"):setBtnNameReact("Login");
-          }}>{btnNameReact}</button>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
