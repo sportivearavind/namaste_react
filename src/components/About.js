fd62c0e1187d2745a626d242632a0dd59dd977f8
Aravind <aravind16101800@gmail.com>
2024-05-11 15:57:05 +0530

    feat: Lifecycle of react based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,41 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () => {
-    return(
-        <div>
-            <h1>About</h1>
-            <h2>This is the about section</h2>
-            <User name={"Aravind from function"} />
-            <UserClass name={"Aravind from Class"} />
-        </div>
+//Class based Component
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent ComponentDidMount Called");
+  }
+
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Class Based Component</h1>
+        <h2>This is the about section</h2>
+        <UserClass name={"Aravind"} />
+        <UserClass name={"Dhoni"} />
+      </div>
     );
+  }
 }
+
+//Function based Component
+// const About = () => {
+//     return(
+//         <div>
+//             <h1>About Function Based Component</h1>
+//             <h2>This is the about section</h2>
+//             <User name={"Aravind from function"} />
+//             <UserClass name={"Aravind from Class"} />
+//         </div>
+//     );
+// }
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,11 +6,28 @@ class UserClass extends React.Component{
         super(props);
 
         console.log(props);
+        this.state = {
+            count : 0,
+        }
+        console.log(this.props.name + " Child Constructor");
     }
+
+    componentDidMount(){
+        console.log(this.props.name + " Child ComponentDidMount Called");
+    }
+
     render(){
+        const {count} = this.state;  // destructuring
+        console.log(this.props.name + " Child Render");
         return(
             <div className="user-card">
                 <h2>Name : {this.props.name}</h2>
+                <h2>Count : {count}</h2>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count+1,
+                    })
+                }}>Count Increase</button>
                 <h3>Age : 23</h3>
                 <h3>Location : Chennai</h3>
             </div>
