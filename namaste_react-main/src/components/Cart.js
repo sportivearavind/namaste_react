bc4cad8d326bbd525abdb920fb04d9181b2fcdf5
Aravind <aravind16101800@gmail.com>
2024-05-15 18:42:08 +0530

    feat: Clear Cart feature added

--- namaste_react-main/src/components/Cart.js
+++ namaste_react-main/src/components/Cart.js
@@ -1,14 +1,25 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./itemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () =>{
 
     const cartItems = useSelector((store) => store.cart.items);
 
+    const dispatch = useDispatch();
+    const handleClearCart = () =>{
+        dispatch(clearCart());
+    }
+
     return(
         <div className="text-center p-2 m-2" >
             <h1 className="text-2xl font-bold">Cart</h1>
             <div className="w-6/12 m-auto">
+                <button 
+                    className="p-2 m-2 bg-black text-white rounded-lg"
+                    onClick={handleClearCart}
+                >Clear Cart</button>
+                {cartItems.length === 0 && <h1>Cart is empty, Add Items to the Cart!.</h1>}
                 <ItemList items={cartItems} />
             </div>
         </div>
